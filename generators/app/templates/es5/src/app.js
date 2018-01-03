3f7daf01994870a5db57968f37b8c98727e14efd
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-03 14:29:53 -0800

    Use App in ES5 template

--- generators/app/templates/es5/src/app.js
+++ generators/app/templates/es5/src/app.js
@@ -1,4 +1,16 @@
 var Stage = require ("openfl/display/Stage").default;
 
-var stage = new Stage (550, 400, 0xFFFFFF);
+
+var App = function () {
+	
+	Sprite.call (this);
+	
+	
+	
+}
+
+App.prototype = Sprite.prototype;
+
+
+var stage = new Stage (550, 400, 0xFFFFFF, App);
 document.body.appendChild (stage.element);
